0b7294075b3055ebce3c9bd835bb4e578415d7c8
Yixing Lao <yixing.lao@gmail.com>
2019-08-20 02:00:12 -0700

    first working bridge

---
+++ bridge.cc
@@ -0,0 +1,46 @@
+#include <dlfcn.h>
+#include <iostream>
+#include <string>
+
+#include "bridge.h"
+#include "point.h"
+
+void* GetLibHandle() {
+    static void* handle = nullptr;
+    static const std::string lib_name = "libpoint.so";
+
+    if (!handle) {
+        handle = dlopen(lib_name.c_str(), RTLD_LAZY);
+        if (!handle) {
+            const char* msg = dlerror();
+            throw std::runtime_error("Cannot load " + std::string(msg));
+        }
+    }
+
+    // handle != nullptr guaranteed here
+    return handle;
+}
+
+namespace bridge {
+
+struct Point add_point(struct Point a, struct Point b) {
+    // func_name, func_t and func
+    static const std::string func_name = "add_point";
+    typedef struct Point (*func_t)(struct Point a, struct Point b);
+    static func_t func = nullptr;
+
+    // Load func
+    if (!func) {
+        func = (func_t)dlsym(GetLibHandle(), func_name.c_str());
+        if (!func) {
+            const char* msg = dlerror();
+            throw std::runtime_error("Cannot load " + func_name + ": " +
+                                     std::string(msg));
+        }
+    }
+
+    // Run func, now func != nullptr
+    return func(a, b);
+}
+
+}  // namespace bridge
--- bridge.h
+++ bridge.h
@@ -3,6 +3,8 @@
 // Forward declares
 struct Point;
 
-// struct Point add_point(struct Point a, struct Point b)
-typedef struct Point (*add_point_t)(struct Point, struct Point);
-add_point_t add_point;
+namespace bridge {
+
+struct Point add_point(struct Point a, struct Point b);
+
+}  // namespace bridge
