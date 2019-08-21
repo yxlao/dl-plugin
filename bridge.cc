55edce7cf27596692b7a2365c771cbfb2e7a7426
Yixing Lao <yixing.lao@gmail.com>
2019-08-20 17:22:54 -0700

    support up to 21

--- bridge.cc
+++ bridge.cc
@@ -6,9 +6,7 @@
 #include "bridge.h"
 #include "point.h"
 
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
-
+// clang-format off
 #define ARGS_1(a1, ...) a1
 #define ARGS_2(a1, a2, ...) a2
 #define ARGS_3(a1, a2, a3, ...) a3
@@ -20,9 +18,21 @@
 #define ARGS_9(a1, a2, a3, a4, a5, a6, a7, a8, a9, ...) a9
 #define ARGS_10(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, ...) a10
 #define ARGS_11(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, ...) a11
+#define ARGS_12(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, ...) a12
+#define ARGS_13(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, ...) a13
+#define ARGS_14(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, ...) a14
+#define ARGS_15(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, ...) a15
+#define ARGS_16(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, ...) a16
+#define ARGS_17(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, ...) a17
+#define ARGS_18(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, ...) a18
+#define ARGS_19(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, ...) a19
+#define ARGS_20(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, ...) a20
+#define ARGS_21(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, ...) a21
+// clang-format on
 
-#define COUNT_ARGS(...) \
-    ARGS_11(dummy, ##__VA_ARGS__, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define COUNT_ARGS(...)                                                      \
+    ARGS_21(dummy, ##__VA_ARGS__, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, \
+            8, 7, 6, 5, 4, 3, 2, 1, 0)
 
 // Convert to list of "arg_type arg_name"
 // Converts:
