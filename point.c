82ebaf6740f8cef0a46c206db765271bbc9099fa
Yixing Lao <yixing.lao@gmail.com>
2019-08-20 17:42:17 -0700

    add add_point_three funct

--- point.c
+++ point.c
@@ -7,6 +7,13 @@ struct Point add_point(struct Point a, struct Point b) {
     return c;
 }
 
+struct Point add_point_three(struct Point a, struct Point b, struct Point c) {
+    struct Point d;
+    d.x = a.x + b.x + c.x;
+    d.y = a.y + b.y + c.y;
+    return d;
+}
+
 struct Point sub_point(struct Point a, struct Point b) {
     struct Point c;
     c.x = a.x - b.x;
