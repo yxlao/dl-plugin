947889daf5759348976ba7b6511f332f7e84e3ab
Yixing Lao <yixing.lao@gmail.com>
2019-08-23 02:11:58 -0700

    new counting code for windows

--- bridge.cc
+++ bridge.cc
@@ -10,8 +10,6 @@
 #include "bridge.h"
 #include "point.h"
 
-#define EXPAND(x) x
-
 // clang-format off
 #define ARGS_1(a1, ...) a1
 #define ARGS_2(a1, a2, ...) a2
@@ -36,11 +34,20 @@
 #define ARGS_21(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, ...) a21
 // clang-format on
 
-// #define COUNT_ARGS(...) (sizeof((int[]){EXPAND(__VA_ARGS__)}) / sizeof(int))
+#ifdef _MSC_VER
 
-#define COUNT_ARGS(...)                                                     \
-    ARGS_21(dummy, EXPAND(__VA_ARGS__), 19, 18, 17, 16, 15, 14, 13, 12, 11, \
-            10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define EXPAND(x) x
+#define __NARGS(_1, _2, _3, _4, _5, VAL, ...) VAL
+#define NARGS_1(...) EXPAND(__NARGS(__VA_ARGS__, 4, 3, 2, 1, 0))
+#define AUGMENTER(...) unused, __VA_ARGS__
+#define COUNT_ARGS(...) NARGS_1(AUGMENTER(__VA_ARGS__))
+
+#else
+
+#define COUNT_ARGS(...) __NARGS(0, ##__VA_ARGS__, 5, 4, 3, 2, 1, 0)
+#define __NARGS(_0, _1, _2, _3, _4, _5, N, ...) N
+
+#endif
 
 // Convert to list of "arg_type arg_name"
 // Converts:
